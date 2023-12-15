d501ae1722867fa2647ab1cea4cd2ec9938f3aae
abhiobourne <jatinjadonjj@gmail.com>
2023-12-16 00:11:12 +0530

    Cart option added

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,34 @@
 "use client"
 import { PRODUCT_CATEGORIES } from "@/config"
 import { useState } from "react"
+import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
+import { useRef, useEffect } from "react"
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null)
-    return 
-        <div className="flex gap-4 h-full">
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if(e.key === "Escape") {
+                setActiveIndex(null)
+            }
+        }
+        document.addEventListener("keydown", handler)
+
+        return () => {
+            document.removeEventListener("keydown", handler)
+        }
+    }, [])
+
+
+    const isAnyOpen = activeIndex !== null
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+    return( 
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
                 const handleOpen = () => {
                     if(activeIndex === i) {
@@ -15,11 +38,18 @@ const NavItems = () => {
                     }
                 }
                 const isOpen = i === activeIndex
-                return (
-                    <NavItem />
+                return (<NavItem 
+                    category={category} 
+                    handleOpen={handleOpen}
+                    isOpen = {isOpen}
+                    key = {category.value}
+                    isAnyOpen={isAnyOpen}
+                    />
                 )
+                
             })}
         </div>
+    )
     
 }
 
